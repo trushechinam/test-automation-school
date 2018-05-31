a060d8439b2de46e71c271bdbc8e1d365175f9c3
mehonoshina <trushechinam@inbox.lv>
2018-05-31 12:30:26 +0300

    test

--- module1/src/test/java/test/automation/school/Test.java
+++ module1/src/test/java/test/automation/school/Test.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Test {
     public static void main(String[] args) {
         ChromeDriver chromeDriver = new ChromeDriver();
-        System.out.println("new");
+        System.out.println("new 111");
     }
 
 
